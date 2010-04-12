7c5a3adb80933087431c0cd2a7faa9194fdc413b
chindemi.g <chindemi.g@6ad24d80-1c63-b969-a4c0-2d43d7d09860>
2010-04-12 18:47:24 +0000

    Risultato Test: La Texture M garantisce risultati migliori della Global M a patto di usarla correttamente, cosa non banale.

--- trunk/proveCUDA/GlobalVSTexture_kernel.cu
+++ trunk/proveCUDA/GlobalVSTexture_kernel.cu
@@ -4,82 +4,99 @@
 #include <stdio.h>
 #include <math.h>
 
-#define BLOCK_DIM 128
+#include "cuPrintf.cu"
+
+#define BLOCK_DIM 384
 
 
 // declare texture reference for 1D float texture
 texture<float, 1, cudaReadModeElementType> tex;
 
 
-__global__ void textureKernel (double* polarization, int* neighbours, int polarization_dim, int neighbours_dim)
+__global__ void textureKernel (float* result, int* neighbours, int result_dim, int neighbours_dim)
 {
 
- if ( blockIdx.x*blockDim.x + threadIdx.x < polarization_dim ) {
+ if ( blockIdx.x*blockDim.x + threadIdx.x < result_dim ) {
    
       int i, num_neighbours;
-      double my_polarization, neighbours_polarization; 
+      float my_polarization, neighbours_polarization; 
 
-      num_neighbours = neighbours_dim / polarization_dim;
+      num_neighbours = neighbours_dim / result_dim;
 
-      my_polarization = polarization[blockIdx.x*blockDim.x + threadIdx.x];
+      my_polarization = tex1D(tex, blockIdx.x*blockDim.x + threadIdx.x);
 
       neighbours_polarization = 0;
       for (i = 0; i < num_neighbours; i++)
 	 neighbours_polarization += tex1D(tex, neighbours[blockIdx.x*blockDim.x + threadIdx.x*num_neighbours + i]);
 
-      my_polarization *= neighbours_polarization;
-
-      polarization[blockIdx.x*blockIdx.x + threadIdx.x] = my_polarization;
+      result[blockIdx.x*blockDim.x + threadIdx.x] = my_polarization * neighbours_polarization;
 
    }
 
 }
 
 
-__global__ void globalKernel (double* polarization, int* neighbours, int polarization_dim, int neighbours_dim)
+__global__ void globalKernel (float *result, float *polarization, int *neighbours, int result_dim, int polarization_dim, int neighbours_dim)
 {
 
-   if ( blockIdx.x*blockDim.x + threadIdx.x < polarization_dim ) {
-   
-      int i, num_neighbours;
-      double my_polarization, neighbours_polarization; 
+   int index = blockIdx.x*blockDim.x + threadIdx.x;
+   int i, num_neighbours, index_next;
+   float my_polarization, neighbours_polarization = 0; 
 
+   //cuPrintf("Index: %d\n", index, polarization_dim);
+
+   if ( index < polarization_dim ) {
+      
       num_neighbours = neighbours_dim / polarization_dim;
 
-      my_polarization = polarization[blockIdx.x*blockDim.x + threadIdx.x];
+      //cuPrintf("Number Neigh.: %d\n", num_neighbours);
 
-      neighbours_polarization = 0;
+      my_polarization = polarization[index];
+
+      //cuPrintf("My polarization: %f\n", my_polarization);
+
       for (i = 0; i < num_neighbours; i++)
-	 neighbours_polarization += polarization[ neighbours[blockIdx.x*blockDim.x + threadIdx.x*num_neighbours + i] ];
+      {
+         index_next = neighbours[index * num_neighbours + i];
+         //cuPrintf("Next neighbour index: %d\n", index_next);
 
-      my_polarization *= neighbours_polarization;
+	 neighbours_polarization += polarization[index_next];
+         //cuPrintf("Next neighbour pol: %f\n", polarization[index_next]);
+      }
 
-      polarization[blockIdx.x*blockIdx.x + threadIdx.x] = my_polarization;
+      result[index] = my_polarization * neighbours_polarization;
+
+      //cuPrintf("Result: %f\n", result[index]);
 
    }
+
 
 }
 
 
 extern "C"
-void launchGlobalKernel (double* h_polarization, int* h_neighbours, int h_polarization_dim, int h_neighbours_dim) {
+void launchGlobalKernel (float *h_result, float *h_polarization, int *h_neighbours, int h_result_dim, int h_polarization_dim, int h_neighbours_dim) {
 
    // Declarations
    float milliseconds;
    cudaEvent_t startEvent, stopEvent;
-   double* d_polarization;
-   int* d_neighbours;	
+   float *d_polarization, *d_result;
+   int *d_neighbours;	
 
    // Setup execution parameters
    dim3 threads(BLOCK_DIM);
-   dim3 grid(ceil(h_polarization_dim/BLOCK_DIM));
+   dim3 grid(ceil ((float)h_polarization_dim/BLOCK_DIM));
+
+   printf("threads per block: %d\nblocks per grid: %d\n", threads.x, grid.x);
 
    cudaSetDevice( cutGetMaxGflopsDeviceId() );
+   cudaPrintfInit ();
 
-   cutilSafeCall(cudaMalloc((void**) &d_polarization, h_polarization_dim*sizeof(double))); 
+   cutilSafeCall(cudaMalloc((void**) &d_result, h_result_dim*sizeof(float))); 
+   cutilSafeCall(cudaMalloc((void**) &d_polarization, h_polarization_dim*sizeof(float))); 
    cutilSafeCall(cudaMalloc((void**) &d_neighbours, h_neighbours_dim*sizeof(int))); 
 
-   cutilSafeCall(cudaMemcpy(d_polarization, h_polarization, h_polarization_dim*sizeof(double), cudaMemcpyHostToDevice));
+   cutilSafeCall(cudaMemcpy(d_polarization, h_polarization, h_polarization_dim*sizeof(float), cudaMemcpyHostToDevice));
    cutilSafeCall(cudaMemcpy(d_neighbours, h_neighbours, h_neighbours_dim*sizeof(int), cudaMemcpyHostToDevice));
 
    printf("Inizio kernel...\n");
@@ -89,7 +106,7 @@ void launchGlobalKernel (double* h_polarization, int* h_neighbours, int h_polari
    cutilSafeCall(cudaEventRecord(startEvent, 0));
 
    // Execute the kernel
-   globalKernel<<< grid, threads >>> (d_polarization, d_neighbours, h_polarization_dim, h_neighbours_dim);
+   globalKernel<<< grid, threads >>> (d_result, d_polarization, d_neighbours, h_result_dim, h_polarization_dim, h_neighbours_dim);
 
    // Wait Device
    cudaThreadSynchronize();
@@ -99,8 +116,12 @@ void launchGlobalKernel (double* h_polarization, int* h_neighbours, int h_polari
    cutilSafeCall(cudaEventElapsedTime(&milliseconds, startEvent, stopEvent));
    
    printf("Fine kernel\n");
+
+   cudaPrintfDisplay(stdout, true);
+
+   cudaPrintfEnd();
    
-   cutilSafeCall(cudaMemcpy(h_polarization, d_polarization, h_polarization_dim*sizeof(double), cudaMemcpyDeviceToHost));
+   cutilSafeCall(cudaMemcpy(h_result, d_result, h_result_dim*sizeof(float), cudaMemcpyDeviceToHost));
 
    printf("Tempo d'esecuzione su GPU (Global Memory): %f ms.\n", milliseconds);
 
@@ -108,13 +129,13 @@ void launchGlobalKernel (double* h_polarization, int* h_neighbours, int h_polari
 
 
 extern "C"
-void launchTextureKernel (double* h_polarization, int* h_neighbours, int h_polarization_dim, int h_neighbours_dim) {
+void launchTextureKernel (float *h_result, float *h_polarization, int *h_neighbours, int h_result_dim, int h_polarization_dim, int h_neighbours_dim) {
 
    // Declarations
    float milliseconds;
    cudaEvent_t startEvent, stopEvent;
-   double* d_polarization;
-   int* d_neighbours;	
+   float *d_result;
+   int *d_neighbours;	
 
    // Setup execution parameters
    dim3 threads(BLOCK_DIM);
@@ -123,7 +144,7 @@ void launchTextureKernel (double* h_polarization, int* h_neighbours, int h_polar
    cudaSetDevice( cutGetMaxGflopsDeviceId() );
 
    // allocate device memory for result
-   cutilSafeCall(cudaMalloc((void**) &d_polarization, h_polarization_dim*sizeof(double))); 
+   cutilSafeCall(cudaMalloc((void**) &d_result, h_result_dim*sizeof(float))); 
    cutilSafeCall(cudaMalloc((void**) &d_neighbours, h_neighbours_dim*sizeof(int))); 
 
    cutilSafeCall(cudaMemcpy(d_neighbours, h_neighbours, h_neighbours_dim*sizeof(int), cudaMemcpyHostToDevice));
@@ -149,7 +170,7 @@ void launchTextureKernel (double* h_polarization, int* h_neighbours, int h_polar
    cutilSafeCall(cudaEventRecord(startEvent, 0));
 
    // Execute the kernel
-   textureKernel<<< grid, threads >>> (d_polarization, d_neighbours, h_polarization_dim, h_neighbours_dim);
+   textureKernel<<< grid, threads >>> (d_result, d_neighbours, h_result_dim, h_neighbours_dim);
 
    // Wait Device
    cudaThreadSynchronize();
@@ -160,7 +181,7 @@ void launchTextureKernel (double* h_polarization, int* h_neighbours, int h_polar
    
    printf("Fine kernel\n");
 
-   cutilSafeCall(cudaMemcpy(h_polarization, d_polarization, h_polarization_dim*sizeof(double), cudaMemcpyDeviceToHost));
+   cutilSafeCall(cudaMemcpy(h_result, d_result, h_result_dim*sizeof(float), cudaMemcpyDeviceToHost));
 
    printf("Tempo d'esecuzione su GPU (Texture Memory): %f ms.\n", milliseconds);
 
