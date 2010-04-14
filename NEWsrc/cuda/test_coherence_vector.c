891aa2d705d93de1cfbc6fc06c58d0b3266347d2
chindemi.g <chindemi.g@6ad24d80-1c63-b969-a4c0-2d43d7d09860>
2010-04-14 16:46:09 +0000

    Modifiche a coherence_vector.cu

---
+++ NEWsrc/cuda/test_coherence_vector.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define NUM_CELLS 100
+#define NUM_NEIGH_FIX 4
+#define __DEBUG_MODE__
+
+
+//-- Generates a psuedo-random float between min and max
+float randfloat (float min, float max)
+{
+   if (min>max) 
+   {
+      return rand()/((float)RAND_MAX+1)*(min-max)+max;    
+   } 
+   else 
+   {
+      return rand()/((float)RAND_MAX+1)*(max-min)+min;
+   }
+}
+
+
+//------------------------------------------------------------------------------------ 
+
+
+//-- Generates a psuedo-random integer between min and max
+int randint(int min, int max)
+{
+   return (rand()%(int)(((max)+1)-(min)))+(min);
+}
+
+
+//------------------------------------------------------------------------------------
+
+
+int main () 
+{
+   
+   int i, j;
+   float polarization[NUM_CELLS], clock[NUM_CELLS], lambda_x[NUM_CELLS], lambda_y[NUM_CELLS], lambda_z[NUM_CELLS];
+   float Ek[NUM_CELLS][NUM_NEIGH_FIX];
+   int neighbours[NUM_CELLS][NUM_NEIGH_FIX];
+   int iterations = 1;
+
+   // Generate random polarizations, clock, lambda
+   for (i = 0; i < NUM_CELLS; i++)
+   {
+      polarization[i] = randfloat(0, 10);
+      clock[i] = randfloat(0, 1);
+      lambda_x[i] = randfloat(0, 1);
+      lambda_y[i] = randfloat(0, 1);
+      lambda_z[i] = randfloat(0, 1);
+   }
+
+   // Generate random Ek
+   for (i = 0; i < NUM_CELLS; i++)
+      for (j = 0; j < NUM_NEIGH_FIX; j++)
+	 Ek[i][j] = randfloat(0, 10);
+      
+   // Generate random neighbours
+   for (i = 0; i < NUM_CELLS; i++)
+      for (j = 0; j < NUM_NEIGH_FIX; j++)
+	 neighbours[i][j] = randint(0, NUM_CELLS-1);
+
+#ifdef __DEBUG_MODE__
+   FILE *fp;
+
+   fp = fopen("log.txt", "w");
+   fprintf(fp,"Index\t||\tPolarization\t||\tClock\t||\tLambda\t||\tNeighbours\t||\tEk\n");
+   for (i = 0; i < NUM_CELLS; i++)
+   {
+      fprintf(fp, "%5d\t||\t%03.04f\t||\t%f\t||\t%f %f %f\t||\t", i, polarization[i], clock[i], lambda_x[i], lambda_y[i], lambda_z[i]);
+      for (j = 0; j < NUM_NEIGH_FIX; j++)
+      {
+	 fprintf(fp, "%04d ", neighbours[i][j]);
+      }
+      
+      fprintf(fp, "\t||\t");
+
+      for (j = 0; j < NUM_NEIGH_FIX; j++)
+      {
+	 fprintf(fp, "%f ", Ek[i][j]);
+      }
+
+      fprintf(fp, "\n");
+   }
+   fclose(fp);
+
+   // Check Consistency
+   for (i = 0; i < NUM_CELLS; i++)
+      for (j = 0; j < NUM_NEIGH_FIX; j++)
+	 if ( !(0 <= neighbours[i][j] && neighbours[i][j] < NUM_CELLS) )
+	 {
+	    printf ("Consistency check on neighbours failed.\n");
+	    return -1;
+	 }
+   printf ("Consistency check on neighbours OK.\n");
+#endif
+
+   // Launch Simulation
+   launch_coherence_vector_simulation (polarization, clock, lambda_x, lambda_y, lambda_z, Ek, neighbours, NUM_CELLS, NUM_NEIGH_FIX, iterations);
+   
+   return 0;
+
+}
+
+
+
