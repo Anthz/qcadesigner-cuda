a200f64a761d4535991e982c9ab0a5a7281517cc
Gibbo87 <Gibbo87@6ad24d80-1c63-b969-a4c0-2d43d7d09860>
2010-04-16 17:22:22 +0000

    I forgot some files...

---
+++ NEWsrc/test_conversion.c
@@ -0,0 +1,62 @@
+#include "bistable_simulation.h"
+#include "conversion.h"
+#include "objects/QCADCell.h"
+
+
+//common variables
+float *h_polarization, *h_clock, **h_Ek;
+int **h_neighbours, cells_number=0,i,j;
+
+extern void test_conversion(QCADCell*** sorted_cells, int number_of_cell_layers, int *number_of_cells_in_layer){
+  
+  //find cells number
+  for( i=0; i<number_of_cell_layers; i++){
+    cells_number += number_of_cells_in_layer[i];
+    printf("layer %d, celle %d\n",i,number_of_cells_in_layer[i]);
+  }
+  
+  //allocate structures
+  sorted_cells_to_CUDA_Structures(sorted_cells,&h_polarization,&h_clock,&h_Ek,&h_neighbours,cells_number, number_of_cell_layers, number_of_cells_in_layer);
+  
+ int neighbours_number = 0,iLayer,iCell;
+ 
+ //init neighbours_number
+ for( iLayer = 0; iLayer < number_of_cell_layers; iLayer++){
+    for (iCell = 0; iCell < number_of_cells_in_layer[iLayer];iCell++){
+      if(neighbours_number < ((bistable_model *)sorted_cells[iLayer][iCell]->cell_model)->number_of_neighbours)
+	neighbours_number = ((bistable_model *)sorted_cells[iLayer][iCell]->cell_model)->number_of_neighbours;
+    }
+ }
+
+  printf("\ncells: %d, layers: %d cells per layer 0: %d, neighbours: %d",cells_number,number_of_cell_layers, number_of_cells_in_layer[0],neighbours_number);
+  //look at the structure.
+  printf("Polarization:\n");
+  for(i =0;i< cells_number;i++){
+    printf("%d: %f\n",i,h_polarization[i]);
+  }
+  
+  printf("\nClock:\n");
+  for( i =0;i< cells_number;i++){
+    printf("%d: %f\n",i,h_clock[i]);
+  }
+  
+  printf("\nKink Energy:\n");
+  for( i =0;i< cells_number;i++){
+    printf("%d: ",i);
+    for( j=0;j< neighbours_number;j++){
+      printf("%f ",h_Ek[i][j]);
+    }
+    printf("\n");
+  }
+  
+  printf("\nNeighbours:\n");
+  for( i =0;i< cells_number;i++){
+    printf("%d: ",i);
+    for( j=0;j< neighbours_number;j++){
+      printf("%d ",h_neighbours[i][j]);
+    }
+    printf("\n");
+  }
+  system("PAUSE");
+}
+
