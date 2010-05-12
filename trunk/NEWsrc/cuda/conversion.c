b8d391f7bb21e28825d00bf0b61cd688a2db18db
Gibbo87 <Gibbo87@6ad24d80-1c63-b969-a4c0-2d43d7d09860>
2010-05-12 15:49:41 +0000

    removed test_conversion

--- trunk/NEWsrc/cuda/conversion.c
+++ trunk/NEWsrc/cuda/conversion.c
@@ -4,12 +4,16 @@
 
  int *tmp;
 
-extern void sorted_cells_to_CUDA_Structures(QCADCell ***sorted_cells, float **h_polarization, float **h_clock, float ***h_Ek, int ***h_neighbours, int cells_number, int number_of_cell_layers, int *number_of_cells_in_layer){
+extern void sorted_cells_to_CUDA_Structures(QCADCell ***sorted_cells, float **h_polarization, float **h_clock, float ***h_Ek, int ***h_neighbours, int number_of_cell_layers, int *number_of_cells_in_layer){
   
  // Allocate memory for all needed structures
  int i,iLayer, iCell, iNeighbour;
  int neighbours_number = 0;
  int counter = 0;
+ int cells_number=0;
+
+ for(i = 0; i < number_of_cell_layers; i++)
+    cells_number+= number_of_cells_in_layer[i];
 
 tmp = (int*)malloc(cells_number*sizeof(int));
  
